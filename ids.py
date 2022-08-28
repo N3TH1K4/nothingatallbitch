ff397445f8748ff87af20422b729cb4c80b5aa5c
N3TH1K4 <88619598+N3TH1K4@users.noreply.github.com>
2022-08-28 21:46:10 +0530

    Update ids.py

--- ids.py
+++ ids.py
@@ -6,125 +6,125 @@ def id_fetch(idw):
         return ida
 
     elif idw == "002":
-            ida = w.texas2
+        ida = w.texas2
         return ida
 
     elif idw == "003":
-            ida = w.texas3
+        ida = w.texas3
         return ida
 
     elif idw == "004":
-            ida = w.texas4
+        ida = w.texas4
         return ida
 
     elif idw == "005":
-            ida = w.texas5
+        ida = w.texas5
         return ida
 
     elif idw == "006":
-            ida = w.texas6
+        ida = w.texas6
         return ida
 
     elif idw == "007":
-            ida = w.texas7
+        ida = w.texas7
         return ida
 
     elif idw == "008":
-            ida = w.hoshiguma1
+        ida = w.hoshiguma1
         return ida
 
     elif idw == "009":
-            ida = w.hoshiguma2
+        ida = w.hoshiguma2
         return ida
 
     elif idw == "010":
-            ida = w.hoshiguma3
+        ida = w.hoshiguma3
         return ida
 
     elif idw == "011":
-            ida = w.hoshiguma4
+        ida = w.hoshiguma4
         return ida
 
     elif idw == "012":
-            ida = w.exu1
+        ida = w.exu1
         return ida
 
     elif idw == "013":
-            ida = w.dobermann1
+        ida = w.dobermann1
         return ida
 
     elif idw == "014":
-            ida = w.dobermann2
+        ida = w.dobermann2
         return ida
 
     elif idw == "015":
-            ida = w.dobermann3
+        ida = w.dobermann3
         return ida
 
     elif idw == "016":
-            ida = w.dobermann4
+        ida = w.dobermann4
         return ida
 
     elif idw == "017":
-            ida = w.gravel1
+        ida = w.gravel1
         return ida
 
     elif idw == "018":
-            ida = w.gravel2
+        ida = w.gravel2
         return ida
 
     elif idw == "019":
-            ida = w.shirayuki1
+        ida = w.shirayuki1
         return ida
 
     elif idw == "020":
-            ida = w.shirayuki2
+        ida = w.shirayuki2
         return ida
 
     elif idw == "021":
-            ida = w.shirayuki3
+        ida = w.shirayuki3
         return ida
 
     elif idw == "022":
-            ida = w.shirayuki4
+        ida = w.shirayuki4
         return ida
 
     elif idw == "023":
-            ida = w.shirayuki5
+        ida = w.shirayuki5
         return ida
 
     elif idw == "024":
-            ida = w.shirayuki6
+        ida = w.shirayuki6
         return ida
 
     elif idw == "025":
-            ida = w.orchid1
+        ida = w.orchid1
         return ida
 
     elif idw == "026":
-            ida = w.melantha1
+        ida = w.melantha1
         return ida
 
     elif idw == "027":
-            ida = w.mousse1
+        ida = w.mousse1
         return ida
 
     elif idw == "028":
-            ida = w.popukar1
+        ida = w.popukar1
         return ida
 
     elif idw == "029":
-            ida = w.popukar2
+        ida = w.popukar2
         return ida
 
     elif idw == "030":
-            ida = w.popukar3
+        ida = w.popukar3
         return ida
 
     elif idw == "031":
-            ida = w.catapult1
+        ida = w.catapult1
         return ida
 
     elif idw == "032":
-            ida = w.texas8
+        ida = w.texas8
         return ida
